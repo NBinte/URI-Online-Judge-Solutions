0577608cc55aaac6a0825bef74334082774e512d
NBinte <nuzhatbintei@gmail.com>
2021-01-30 23:22:15 +0600

    Initial commit of Beginner 1004.cpp

---
+++ Beginner/C++/1004.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int  A, B;
+
+    cin>>A;
+
+    cin>>B;
+
+    int PROD = A * B;
+
+    cout<<"PROD = "<<PROD<<endl;
+
+    return 0;
+}
+
